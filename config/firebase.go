abdc194a537497fb4500f820b3a32f4ad972b16e
alongkornpp <alongkorn.p@cloudnctech.com>
2024-09-09 11:14:33 +0700

    modify: config folder

--- config/firebase.go
+++ config/firebase.go
@@ -1,29 +1,41 @@
 package config
 
 import (
-    "context"
-    "firebase.google.com/go"
-    "firebase.google.com/go/db"
-    "log"
-    _ "golang.org/x/oauth2/google"
-    "google.golang.org/api/option"
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/db"
+	_ "golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
 )
 
 var Client *db.Client
 
 func InitFirebase() {
-    // โหลด serviceAccountKey.json
-    sa := option.WithCredentialsFile("/Users/alongkorn/Desktop/gamevr-88a69-firebase-adminsdk-ukt0n-90b587a9b3.json")
+	// โหลด serviceAccountKey.json 
+	sa := option.WithCredentialsFile("/Users/alongkorn/Desktop/gamevr-88a69-firebase-adminsdk-ukt0n-a862e722f6.json")
 
-    // สร้าง App Firebase
-    app, err := firebase.NewApp(context.Background(), nil, sa)
-    if err != nil {
-        log.Fatalf("error initializing app: %v\n", err)
-    }
+	// สร้าง Firebase App
+	app, err := firebase.NewApp(context.Background(), nil, sa)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
 
-    // เริ่มต้นฐานข้อมูล
-    Client, err = app.Database(context.Background())
-    if err != nil {
-        log.Fatalf("error initializing database client: %v\n", err)
-    }
+	// สร้าง Firestore Client
+	client, err := app.Firestore(context.Background())
+	if err != nil {
+		log.Fatalf("error initializing database client: %v\n", err)
+	}
+	defer client.Close()
+
+	// ทดสอบการเชื่อมต่อกับ Firestore
+	_, _, err = client.Collection("testCollection").Add(context.Background(), map[string]interface{}{
+		"testField": "testValue",
+	})
+	if err != nil {
+		log.Fatalf("Failed to add data to Firestore: %v\n", err)
+	}
+
+	log.Println("Successfully added data to Firestore")
 }
