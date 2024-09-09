926128faced644aad914a245a0fcd3997e928950
alongkornpp <alongkorn.p@cloudnctech.com>
2024-09-09 11:20:46 +0700

    clear

--- internal/auth/dto/auth.dto.go
+++ internal/auth/dto/auth.dto.go
@@ -1,15 +1,2 @@
 package dto
 
-type RegisterDTO struct {
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	Class     string `json:"class"`
-	Number    string `json:"number"`
-}
-
-type LoginDTO struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
