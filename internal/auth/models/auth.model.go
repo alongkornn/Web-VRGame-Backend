2c74b7c8640e3658fc7db1e5d0c29ce73cc63ef5
alongkornpp <alongkorn.p@cloudnctech.com>
2024-09-16 11:55:34 +0700

    add: field in user model

--- internal/auth/models/auth.model.go
+++ internal/auth/models/auth.model.go
@@ -21,20 +21,22 @@ const (
 )
 
 type User struct {
-	ID         string    `json:"id" firestore:"id"`
-	FirstName  string    `json:"firstname" firestore:"firstname"`
-	LastName   string    `json:"lastname" firestore:"lastname"`
-	Email      string    `json:"email" firestore:"email"`
-	Password   string    `json:"password" firestore:"password"`
-	Class      string    `json:"class" firestore:"class"`
-	Number     string    `json:"number" firestore:"number"`
-	Score      int       `json:"score" firestore:"score"`
-	Level      int       `json:"level" firestore:"level"`
-	Role       Role      `json:"role" firestore:"role"`
-	Status     Status    `json:"status" firestore:"status"`
-	CreatedAt  time.Time `json:"created_at" firestore:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at" firestore:"updated_at"`
-	Is_Deleted bool      `json:"is_deleted" firestore:"is_deleted"`
+	ID                   string    `json:"id" firestore:"id"`
+	FirstName            string    `json:"firstname" firestore:"firstname"`
+	LastName             string    `json:"lastname" firestore:"lastname"`
+	Email                string    `json:"email" firestore:"email"`
+	Password             string    `json:"password" firestore:"password"`
+	Class                string    `json:"class" firestore:"class"`
+	Number               string    `json:"number" firestore:"number"`
+	Score                int       `json:"score" firestore:"score"`
+	Level                int       `json:"level" firestore:"level"`
+	Role                 Role      `json:"role" firestore:"role"`
+	Status               Status    `json:"status" firestore:"status"`
+	CurrentCheckpoint    string    `firestore:"current_checkpoint"`
+	CompletedCheckpoints []string  `firestore:"completed_checkpoints"`
+	CreatedAt            time.Time `json:"created_at" firestore:"created_at"`
+	UpdatedAt            time.Time `json:"updated_at" firestore:"updated_at"`
+	Is_Deleted           bool      `json:"is_deleted" firestore:"is_deleted"`
 }
 
 func GenerateToken(user *User) (string, error) {
