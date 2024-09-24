a813dbbef68dd03598b4382d3d9db3c9eb6fddd2
alongkornpp <alongkorn.p@cloudnctech.com>
2024-09-24 11:14:00 +0700

    modify: comment in checkpoint

--- internal/checkpoint/routes/checkpoint.route.go
+++ internal/checkpoint/routes/checkpoint.route.go
@@ -8,10 +8,16 @@ import (
 func CheckpointRoute(g *echo.Group) {
 	checkpointGroup := g.Group("/checkpoint")
 
+	// แสดงด่านปัจจุบันโดยเข้าถึงผ่านไอดีผู้เล่น
 	checkpointGroup.POST("/set/:checkpoint/:user", controllers.GetCurrentCheckpointFromUser)
+	// แสดงทุกด่านทุกหมวดหมู่
 	checkpointGroup.POST("/get", controllers.GetAllCheckpoint)
+	// สร้างด่านใหม่
 	checkpointGroup.POST("/create", controllers.CreateCheckpoint)
+	// บันทึกด่านปัจจุบันลงในด่านที่เล่นผ่านแล้ว
 	checkpointGroup.POST("/save/checkpoint/complete/:userId", controllers.SaveCheckpointToComplete)
+	// แสดงด่านที่เล่นผ่านโดยเข้าถึงผ่านไอดีผู้เล่น
 	checkpointGroup.POST("/get/checkpoint/complete/:userId", controllers.GetCompleteCheckpointByUserId)
+	// แสดงด่านทุกด่านตามหมวดหมู่
 	checkpointGroup.POST("/get/category", controllers.GetCheckpointWithCategory)
 }
