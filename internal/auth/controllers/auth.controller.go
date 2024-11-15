b6a6b3eefa487f444b76453d8b935e6774299b53
alongkornpp <alongkorn.p@cloudnctech.com>
2024-11-15 13:34:48 +0700

    increse token key in config.yml and keep token on localstorage with session

--- internal/auth/controllers/auth.controller.go
+++ internal/auth/controllers/auth.controller.go
@@ -3,9 +3,11 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/alongkornn/Web-VRGame-Backend/config"
 	"github.com/alongkornn/Web-VRGame-Backend/internal/auth/dto"
 	"github.com/alongkornn/Web-VRGame-Backend/internal/auth/services"
 	"github.com/alongkornn/Web-VRGame-Backend/pkg/utils"
+	"github.com/gorilla/sessions"
 	"github.com/labstack/echo/v4"
 )
 
@@ -24,17 +26,25 @@ func Register(ctx echo.Context) error {
 }
 
 // เข้าสู้ระบบ
+var store = sessions.NewCookieStore([]byte(config.GetEnv("super_secret_key")))
 func Login(ctx echo.Context) error {
 	var loginDTO dto.LoginDTO
 	if err := ctx.Bind(&loginDTO); err != nil {
 		return utils.SendError(ctx, http.StatusBadRequest, "Invalid input", nil)
 	}
 
-	data, status, err := services.Login(loginDTO.Email, loginDTO.Password, ctx.Request().Context())
+	token, status, err := services.Login(loginDTO.Email, loginDTO.Password, ctx.Request().Context())
 	if err != nil {
 		return utils.SendError(ctx, status, err.Error(), nil)
 	}
-	return utils.SendSuccess(ctx, status, "Successfully to Login", data)
+
+	// บันทึก token ไว้ใน localstorage
+	session, _ := store.Get(ctx.Request(), "authentication")
+
+	session.Values["token"] = token
+    session.Values["authenticated"] = true
+    session.Save(ctx.Request(), ctx.Response())
+	return utils.SendSuccess(ctx, status, "Successfully to Login", nil)
 
 
 
@@ -45,6 +55,6 @@ func Login(ctx echo.Context) error {
 
 
 
-	
+
 }
 
