d37a36d7ef40404a63cc7bae502d1a71fd0a873b
alongkornpp <alongkorn.p@cloudnctech.com>
2024-11-27 15:46:29 +0700

    modify API endpoint

--- internal/user/routes/user.route.go
+++ internal/user/routes/user.route.go
@@ -9,15 +9,15 @@ func UserRoute(e *echo.Group) {
 	userGroup := e.Group("/user")
 
 	// แสดงผู้เล่นแค่คนเดียว
-	userGroup.POST("/get/user/:userId", controllers.GetUserByID)
+	userGroup.GET("/:userId", controllers.GetUserByID)
 	// แสดงผู้เล่นทั้งหมด
-	userGroup.POST("/get/user", controllers.GetAllUser)
+	userGroup.GET("/", controllers.GetAllUser)
 	// แสดงผู้เล่นที่ยังไม่ได้รับการอนุมัติ
-	userGroup.POST("/get/user/pending", controllers.GetUserPending)
+	userGroup.GET("/pending", controllers.GetUserPending)
 	// แก้ไขข้อมูลผู้เล่น
-	userGroup.PUT("/update/user/:userId", controllers.UpdateUser)
+	userGroup.PUT("/:userId", controllers.UpdateUser)
 	// แสดงคะแนนรวมทั้งหมด
-	userGroup.POST("/get/sumsocre/:userId", controllers.GetSumScore)
+	userGroup.GET("/sumsocre/:userId", controllers.GetSumScore)
 	// รวมคะแนนทั้งที่ผู้เล่นทำได้
 	userGroup.POST("/set/sumscore/:userId", controllers.SetSumSocore)
 }
