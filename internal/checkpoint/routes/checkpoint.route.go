fa06dbc09abbd1bae163c2a95945396622e2dc5c
alongkornpp <alongkorn.p@cloudnctech.com>
2025-01-15 18:56:44 +0700

    change enpoint for checkpoint

--- internal/checkpoint/routes/checkpoint.route.go
+++ internal/checkpoint/routes/checkpoint.route.go
@@ -9,15 +9,15 @@ func CheckpointRoute(g *echo.Group) {
 	checkpointGroup := g.Group("/checkpoint")
 
 	// แสดงด่านปัจจุบันโดยเข้าถึงผ่านไอดีผู้เล่น
-	checkpointGroup.POST("/set/:checkpoint/:user", controllers.GetCurrentCheckpointFromUser)
+	checkpointGroup.GET("/current/:user", controllers.GetCurrentCheckpointFromUser)
 	// แสดงทุกด่านทุกหมวดหมู่
-	checkpointGroup.POST("/get", controllers.GetAllCheckpoint)
+	checkpointGroup.GET("/", controllers.GetAllCheckpoint)
 	// สร้างด่านใหม่
 	checkpointGroup.POST("/create", controllers.CreateCheckpoint)
 	// บันทึกด่านปัจจุบันลงในด่านที่เล่นผ่านแล้ว
-	checkpointGroup.POST("/save/checkpoint/complete/:score/:userId", controllers.SaveCheckpointToComplete)
+	checkpointGroup.POST("/save/complete/:score/:userId", controllers.SaveCheckpointToComplete)
 	// แสดงด่านที่เล่นผ่านโดยเข้าถึงผ่านไอดีผู้เล่น
-	checkpointGroup.POST("/get/checkpoint/complete/:userId", controllers.GetCompleteCheckpointByUserId)
+	checkpointGroup.GET("/complete/:userId", controllers.GetCompleteCheckpointByUserId)
 	// แสดงด่านทุกด่านตามหมวดหมู่
-	checkpointGroup.POST("/get/category", controllers.GetCheckpointWithCategory)
+	checkpointGroup.GET("/category", controllers.GetCheckpointWithCategory)
 }
